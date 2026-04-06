a064535fb52de03559c50cc224a189f7cc284f8f
Suikol <3457485859@qq.com>
2026-04-06 11:00:33 +0800

    init

---
+++ flake.nix
@@ -0,0 +1,15 @@
+{
+  description = "Suikol's NixOS flake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+  };
+
+  outputs = { self, nixpkgs, ... }@inputs: {
+    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+      modules = [
+        ./configuration.nix
+      ];
+    };
+  };
+}
