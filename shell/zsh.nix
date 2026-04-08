40a268ec68d90da86ca0b74969673effd5a56d75
Suikol <3457485859@qq.com>
2026-04-08 22:03:39 +0800

    shell: 启用oh-my-zsh并安装一些插件

--- shell/zsh.nix
+++ shell/zsh.nix
@@ -4,5 +4,22 @@
   programs.zsh = {
     enable = true;
     enableCompletion = true;
+    autosuggestion.enable = true; 
+    syntaxHighlighting.enable = true;
+
+    shellAliases = {
+      ll = "ls -l";
+      ff = "fastfetch";
+      vim = "nvim";
+    };
+
+    oh-my-zsh = { 
+      enable = true;
+      plugins = [ 
+        "git"
+	"z"
+      ];
+
+      theme = "robbyrussell"; };
   };
 }
